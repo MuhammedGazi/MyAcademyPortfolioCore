b0ed7f0f50ce5219423bc0880cd8b2db726691e1
MuhammedGazi <muhammed.celenk1@ogr.sakarya.edu.tr>
2025-08-05 22:27:28 +0300

    Add project files.

---
+++ Portfolio.Web/Entities/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Portfolio.Web.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+
+        public IList<Project> Projects { get; set; }
+    }
+}
